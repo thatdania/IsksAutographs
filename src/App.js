7c1ae89eaac76e1fb172e358be6cae269db3c765
thatdania <thatdania@gmail.com>
2018-08-07 19:34:10 +0800

    Added background image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Sample1 from './images/2.jpg';
+import Sample2 from './images/1.jpg';
+import Sample3 from './images/3.jpg';
 import './App.css';
 
 class App extends Component {
@@ -8,8 +11,10 @@ class App extends Component {
         <header className="Isks Autographs">
           <h1 className="title">Loop</h1>
         </header>
+        <img className="Image" src={Sample1} />
+        <img className="Image"src={Sample2} />
+        <img className="Image"src={Sample3} />
         <div className="Price">
-        <img src={"./images/1.jpg"} />
           <p>$5</p>
           <p>$10</p>
           <p>$15</p>
