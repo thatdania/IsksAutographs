ebd61734d1fc99b6f3d664379780035ae1db9d90
thatdania <thatdania@gmail.com>
2018-08-07 19:20:45 +0800

    Added basic content for the website like texts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="Isks Autographs">
+          <h1 className="title">Loop</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Price">
+        <img src={"./images/1.jpg"} />
+          <p>$5</p>
+          <p>$10</p>
+          <p>$15</p>
+        </div> 
+        
+        <button type="button" id="boop" name="boop">boop</button>
+        <p>Inquiries @ YungIskk</p>
       </div>
     );
   }
